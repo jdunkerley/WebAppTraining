dcfa35e7c74a652536d3ef488ff2fea09c8a5373
James Dunkerley <james.dunkerley@gmail.com>
2015-11-10 14:14:03 +0000

    Making QuandlAPI

--- quandlAPI.js
+++ quandlAPI.js
@@ -1,27 +1,59 @@
 (function() {
     'use strict';
     var https = require('https');
+    var concat = require('concat-stream');
     var apiKey = '';
+    var startDate = new Date('2010-01-01');
+    var endDate = new Date('2014-12-31');
 
     function getDataSetUrl(dataset) {
         var url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + dataset + '/data.json';
 
         var params = [];
         if (apiKey !== '') {
-            params.push('')
+            params.push('api_key=' + apiKey);
         }
-        return url;
+        if (startDate) {
+            params.push('start_date=' + startDate.toISOString().substring(0, 10));
+        }
+        if (endDate) {
+            params.push('end_date=' + endDate.toISOString().substring(0, 10));
+        }
+
+        return url + (params.length ? '?' + params.join('&') : '');
     }
 
-    module.exports = function() {
-
+    module.exports = function(dataset) {
+        return new Promise(function(resolve, reject) {
+            https.get(getDataSetUrl(dataset), function(res) {
+                res.pipe(function(buffer) {
+                    resolve(JSON.parse(buffer.toString()));
+                });
+            }).on('error', function(e) {
+                reject(e);
+            });
+        });
     };
+
+    module.getDataSetUrl = getDataSetUrl;
 
     module.apiKey = function(_apiKey) {
         if (!arguments.length) {
             return apiKey;
         }
-
         apiKey = _apiKey;
     };
+    module.startDate = function(_startDate) {
+        if (!arguments.length) {
+            return startDate;
+        }
+        startDate = _startDate;
+    };
+    module.endDate = function(_endDate) {
+        if (!arguments.length) {
+            return endDate;
+        }
+        endDate = _endDate;
+    };
+
 }());
