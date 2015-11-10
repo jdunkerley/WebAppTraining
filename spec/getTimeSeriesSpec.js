93e47c543a79e9be43704753756ffb49cbbfcb2a
James Dunkerley <james.dunkerley@gmail.com>
2015-11-10 14:14:53 +0000

    Revert "Some test and module exports"
    This reverts commit ee4773dec93fa031417591acf9c9c8d3449b41dd.

--- spec/getTimeSeriesSpec.js
+++
@@ -1,9 +0,0 @@
-/* global describe */
-describe('getTimeSeries', function() {
-    var returnsCalc = require('../returnsCalc');
-
-    it('is a function', function() {
-        var typeName = typeof(returnsCalc.getTimeSeries);
-        expect(typeName).toEqual('function');
-    });
-});
