10f641fccd0425ff6ac166dff80e3f344280ff11
James Dunkerley <james.dunkerley@gmail.com>
2015-11-10 16:11:31 +0000

    Todying command line args

--- returnsCmd.js
+++ returnsCmd.js
@@ -20,22 +20,23 @@ if (process.argv.length < 3) {
 else {
     // Arguments DataSet APIKey Start End
     var cli = commandLineArgs([
-        { name: 'dataset', type: String, defaultOption: true },
-        { name: 'apikey', alias: 'k', type: String },
-        { name: 'startDate', alias: 's', type: String },
-        { name: 'endDate', alias: 'e', type: String },
-        { name: 'period', alias: 'p', type: Number },
-        { name: 'periodType', alias: 't', type: String },
+        { name: 'dataset', type: String, defaultOption: true, description: 'Dataset Code within the WIKI database' },
+        { name: 'apikey', alias: 'k', type: String, description: 'Quandl API Key (optional but limited anonymous requests)' },
+        { name: 'startDate', alias: 's', type: String, description: 'Start Date in yyyy-MM-dd (defaults to 2010-01-01)' },
+        { name: 'endDate', alias: 'e', type: String, description: 'End Date in yyyy-MM-dd (defaults to 2014-12-31)' },
+        { name: 'period', alias: 'p', type: Number, description: 'Number of Periods (defaults to 7)' },
+        { name: 'periodType', alias: 't', type: String, description: 'Period Type (defaults to days)' },
         { name: 'help', alias: 'h', type: Boolean }
     ]);
 
     var options = cli.parse();
-    console.log(options);
-
     if (options.help) {
         console.log(cli.getUsage());
         return;
     }
+
+    console.log('Running Returns Calc for: ');
+    console.log(options);
 
     if (options.apikey) {
         quandl.apiKey(options.apikey);
