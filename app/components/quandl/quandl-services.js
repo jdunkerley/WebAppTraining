e7e3611499ea4f4d9a378c8e1d338f9ffad9b8ac
bjedrzejewski <bjedrzejewski@scottlogic.com>
2015-11-13 16:58:59 +0000

    Resyled with inline styles.

--- app/components/quandl/quandl-services.js
+++ app/components/quandl/quandl-services.js
@@ -7,7 +7,7 @@
 
     quandlServices.factory('DatasetsList', ['$resource',
         function ($resource) {
-            return $resource('https://www.quandl.com/api/v3/datasets.json?api_key=yourKey&database_code=WIKI&per_page=:perPage&page=:page', {}, {
+            return $resource('https://www.quandl.com/api/v3/datasets.json?api_key=nzzdYfMQMTZTgeJwsW2k&database_code=WIKI&per_page=:perPage&page=:page', {}, {
                 get: {method: 'GET', cache: true}
             });
         }]);
